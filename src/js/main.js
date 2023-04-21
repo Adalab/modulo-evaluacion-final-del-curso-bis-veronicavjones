343f089c8bbe2d4c2747f9e0e050d0974cb9c3d8
veronicavjones <veronicavjones@hotmail.com>
2023-04-21 18:32:11 +0200

    trying to filter

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,8 @@ const url ='https://api.sampleapis.com/recipes/recipes';
 
 //variable global que guarda los datos
 let listRecipesData = [];
+const filterInput = document.querySelector(".js-filter");
+const favList = document.querySelector(".recipeFavourites");
 
 /*
 1-Obtener los datos: fetch
@@ -19,6 +21,9 @@ fetch(url)
     console.log(data);
     listRecipesData = data;
     renderListRecipes(listRecipes,data);
+    resultRecipes(data);
+    searchRecipe(data);
+    handleBtns();
 })
 
 
@@ -32,33 +37,62 @@ function renderListRecipes(element, recipe){
         element. innerHTML += `<div id= "${recipe[i].id}" class="recipe"> <img class="image" src= ${recipe[i].photoUrl}><br>${recipe[i].title}<br><br>Tipo de comida: ${recipe[i].cuisine}<br>${recipe[i].tags}<br><br class="calories">Calorías: ${recipe[i].calories}</div>`;
     }
     }
-   
-   
-   
-   
-   
-   
-   
-   
-    /*let html = 
-            ` <li>
-                 <article class="recipe">
-                
-                
-                    <img class="photo" src=${recipe.photoUrl} alt="photo">
-                    <h3 class="recipe_title">${recipe.title}</h3>
-                    <ul class="recipes_list">`;
 
+function resultRecipes(data) {
+    for (const recipe of document.querySelectorAll(".recipe")){
+        recipe.addEventListener("click",(ev)=>{
+            const recipeInfo = data.find((recipe)=> {if (recipe.id === parseInt(ev.currentTarget.id)){
+                return recipe;
+            }
+            });
 
-                    for ( const element of recipe.element){
+            favouritesRecipes.push(recipeInfo);
 
+            const newFav = `<li class=" fav js-recipeFavourites" id=${recipeInfo.id}><div id="${recipeInfo.id}" class="recipe"><img class="image" src=${recipeInfo.photoUrl}<br>${recipeInfo.title}<br>Tipo de comida: ${recipeInfo.cuisine}<br>${recipeInfo.tags}<br class="calories">Calorías: ${recipeInfo.calories}</div>`;
+
+            favList.innerHTML += newFav;
+        });
+    }
+}
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    function searchRecipe(food){
+        filterInput.addEventListener("input",(ev)=> {
+            listRecipes.innerHTML = "";
+            const filteredRecipes =[];
+            for (let iFood =0; iFood <food.length; iFood++) {
+                if ( food[iFoods].cuisine
+                    .toLocaleToLowerCase()
+                    .includes(filterInput.value.toLocaleToLowerCase() )
+                    ) {
+                        const recipeInfo = food[iFood];
+                        filteredRecipes.push(recipeInfo);
+                        listRecipes.innerHTML += `<div id="${recipeInfo.id}" class="recipe"><img class="image" src=${recipeInfo.photoUrl}<br>${recipeInfo.title}<br>Tipo de comida: ${recipeInfo.cuisine}<br>${recipeInfo.tags}<br class="calories">Calorías: ${recipeInfo.calories}</div>`;
                     }
-                    <li class="cuisine">${recipe.cuisine}</li>
-                    <li class="tags">${recipe.tags}</li>
-                    <li class="clories">${recipe.calories}</li>
-                    html += `</ul> </article> </li>`;
+            }
+            resultRecipes(filteredRecipes);
+        });
+    }
 
-}*/
+
+    function handleBtns() {
+        saveBtn.addEventListener("click",saveRecipes);
+    }
+   
+   
+   
+   
+   
+   
+   
+   
 
 
 
